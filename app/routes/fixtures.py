be0a041fcea84cd3c6ef3108cb8dc685aae5dd51
YODD95 <136192546+YODD95@users.noreply.github.com>
2026-01-09 12:53:43 +0200

    Create fixtures.py

---
+++ app/routes/fixtures.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, jsonify, request
+import requests
+import os
+
+fixtures_bp = Blueprint("fixtures", __name__)
+
+@fixtures_bp.route("/fixtures", methods=["GET"])
+def fixtures():
+    sport = request.args.get("sport")
+
+    if not sport:
+        return jsonify({
+            "success": False,
+            "error": "Sport is required",
+            "fixtures": []
+        })
+
+    API_KEY = os.environ.get("ODDS_API_KEY")
+
+    url = f"https://api.the-odds-api.com/v4/sports/{sport}/odds"
+    params = {
+        "apiKey": API_KEY,
+        "regions": "eu",
+        "markets": "h2h"
+    }
+
+    res = requests.get(url, params=params)
+    data = res.json()
+
+    return jsonify({
+        "success": True,
+        "fixtures": data
+    })
